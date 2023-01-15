9bb3ea1a154b99854ffdf08dffb66b2f222c4b26
SSuSG <psg980331@naver.com>
2023-01-15 23:37:09 +0900

    feat : 유저 권한 정의(Role)

---
+++ src/main/java/com/toyproject/complaints/domain/user/entity/Role.java
@@ -0,0 +1,21 @@
+package com.toyproject.complaints.domain.user.entity;
+
+public enum Role {
+
+    ADMIN(ROLES.ADMIN , "슈퍼관리자"),
+    USER(ROLES.USER , "회원");
+
+    public static class ROLES{
+        public static final String USER = "ROLE_USER";
+        public static final String ADMIN = "ROLE_ADMIN";
+    }
+
+    private String role;
+    private String description;
+
+    Role(String role, String description) {
+        this.role = role;
+        this.description = description;
+    }
+
+}
