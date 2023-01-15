8e641876cee9ca2ea3c28af648e55e7f5e85a359
SSuSG <psg980331@naver.com>
2023-01-15 23:39:29 +0900

    feat : 유저 생성,수정 시간 정의 클래스

---
+++ src/main/java/com/toyproject/complaints/domain/user/entity/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.toyproject.complaints.domain.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseTimeEntity {
+
+    @CreatedDate
+    protected LocalDateTime createdTime;
+    @LastModifiedDate
+    protected LocalDateTime updatedTime;
+}
