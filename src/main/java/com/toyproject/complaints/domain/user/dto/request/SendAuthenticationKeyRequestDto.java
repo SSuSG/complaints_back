83efa82cb21914547aba0785c2cb6213d1a9fc46
SSuSG <psg980331@naver.com>
2023-01-29 23:15:59 +0900

    feat : 계정잠금해제시 인증번호 요청 DTO

---
+++ src/main/java/com/toyproject/complaints/domain/user/dto/request/SendAuthenticationKeyRequestDto.java
@@ -0,0 +1,14 @@
+package com.toyproject.complaints.domain.user.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class SendAuthenticationKeyRequestDto {
+
+    @NotBlank
+    private String userEmail;
+}
