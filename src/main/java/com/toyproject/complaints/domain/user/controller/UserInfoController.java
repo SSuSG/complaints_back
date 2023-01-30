01c0f0541db9c85e2fe2f57756382bebd8b5ae10
SSuSG <psg980331@naver.com>
2023-01-30 20:58:21 +0900

    feat : IP 삭제 컨트롤러 로직 작성

--- src/main/java/com/toyproject/complaints/domain/user/controller/UserInfoController.java
+++ src/main/java/com/toyproject/complaints/domain/user/controller/UserInfoController.java
@@ -3,12 +3,12 @@ package com.toyproject.complaints.domain.user.controller;
 import com.toyproject.complaints.domain.user.dto.request.ChangeIpRequestDto;
 import com.toyproject.complaints.domain.user.service.UserInfoService;
 import com.toyproject.complaints.global.response.ResponseResult;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -20,10 +20,33 @@ public class UserInfoController {
 
     private final UserInfoService userInfoService;
 
-    //아이피 추가
+    @ApiOperation(value = "아이피 추가" , notes = "슈퍼관리자가 사용자계정에 IP 추가등록을 진행합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "IP 추가 성공"),
+            @ApiResponse(code = 403, message = "관리자권한이 없는 사용자의 접근"),
+            @ApiResponse(code = 406, message = "이미 존재하는 IP는 추가 불가"),
+            @ApiResponse(code = 408, message = "현재 로그인한 관리자를 찾을수 없음"),
+            @ApiResponse(code = 410, message = "등록할 수 있는 IP의 수를 초과"),
+    })
     @PostMapping("/ip")
     public ResponseResult addIpAddress(@Valid @RequestBody ChangeIpRequestDto changeIpRequestDto) {
         if(userInfoService.addIpAddress(changeIpRequestDto)){
+            return ResponseResult.successResponse;
+        }else{
+            return ResponseResult.failResponse;
+        }
+    }
+
+    @ApiOperation(value = "아이피 삭제" , notes = "슈퍼관리자가 사용자계정에 IP 삭제를 진행합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "IP 추가 성공"),
+            @ApiResponse(code = 403, message = "관리자권한이 없는 사용자의 접근"),
+            @ApiResponse(code = 408, message = "현재 로그인한 관리자를 찾을수 없음"),
+            @ApiResponse(code = 410, message = "삭제할 수 있는 IP가 존재하지 않음"),
+    })
+    @DeleteMapping("/ip")
+    public ResponseResult deleteIpAddress(@Valid @RequestBody ChangeIpRequestDto changeIpRequestDto){
+        if(userInfoService.deleteIpAddress(changeIpRequestDto)){
             return ResponseResult.successResponse;
         }else{
             return ResponseResult.failResponse;
