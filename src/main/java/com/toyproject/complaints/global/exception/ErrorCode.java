6386fed36bd37f8bf1582e2399be3b6854504b1c
SSuSG <psg980331@naver.com>
2023-01-30 20:57:35 +0900

    feat : IP 삭제실패(삭제 가능한 IP가 존재하지 않음) 예외 추가

--- src/main/java/com/toyproject/complaints/global/exception/ErrorCode.java
+++ src/main/java/com/toyproject/complaints/global/exception/ErrorCode.java
@@ -20,7 +20,9 @@ public enum ErrorCode {
     EXIST_EMAIL_EXCEPTION(409,"이미 존재하는 이메일 입니다."),
     EXIST_IP_EXCEPTION(409,"이미 존재하는 IP 입니다."),
     EXIST_PHONE_NUMBER_EXCEPTION(409,"이미 존재하는 핸드폰번호 입니다."),
-    N0_CONNECTION_EXCEPTION(410,"연결시간이 초과되었습니다."),
+    FAIL_ADD_IP_BY_OVERSIZE_EXCEPTION(410,"등록할 수 있는 IP의 수는 3개입니다."),
+    FAIL_DELETE_IP_EXCEPTION(410,"삭제할 수 있는 IP가 존재하지 않습니다."),
+    N0_CONNECTION_EXCEPTION(411,"연결시간이 초과되었습니다."),
     SERVER_EXCEPTION(500,"서버에서 예측하지 못한 에러가 발생했습니다.");
 
     private int errorCode;
