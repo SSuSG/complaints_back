a4df38ad16ff36eb7bd5a23018112b25ec701078
SSuSG <psg980331@naver.com>
2023-01-31 21:45:51 +0900

    feat : 모든 유저 조회 컨트롤러 작성

--- src/main/java/com/toyproject/complaints/domain/user/controller/UserInfoController.java
+++ src/main/java/com/toyproject/complaints/domain/user/controller/UserInfoController.java
@@ -1,16 +1,21 @@
 package com.toyproject.complaints.domain.user.controller;
 
 import com.toyproject.complaints.domain.user.dto.request.ChangeIpRequestDto;
+import com.toyproject.complaints.domain.user.dto.response.UserInfoListResponseDto;
 import com.toyproject.complaints.domain.user.service.UserInfoService;
+import com.toyproject.complaints.global.response.ListResponseResult;
 import com.toyproject.complaints.global.response.ResponseResult;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,11 +35,9 @@ public class UserInfoController {
     })
     @PostMapping("/ip")
     public ResponseResult addIpAddress(@Valid @RequestBody ChangeIpRequestDto changeIpRequestDto) {
-        if(userInfoService.addIpAddress(changeIpRequestDto)){
+        if(userInfoService.addIpAddress(changeIpRequestDto))
             return ResponseResult.successResponse;
-        }else{
-            return ResponseResult.failResponse;
-        }
+        return ResponseResult.failResponse;
     }
 
     @ApiOperation(value = "아이피 삭제" , notes = "슈퍼관리자가 사용자계정에 IP 삭제를 진행합니다.")
@@ -46,14 +49,20 @@ public class UserInfoController {
     })
     @DeleteMapping("/ip")
     public ResponseResult deleteIpAddress(@Valid @RequestBody ChangeIpRequestDto changeIpRequestDto){
-        if(userInfoService.deleteIpAddress(changeIpRequestDto)){
+        if(userInfoService.deleteIpAddress(changeIpRequestDto))
             return ResponseResult.successResponse;
-        }else{
-            return ResponseResult.failResponse;
-        }
+        return ResponseResult.failResponse;
     }
 
-    //관리자 리스트 조회
+    @ApiOperation(value = "유저 리스트 조회" , notes = "전체 유저정보 반환")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "전체 유저정보 반환 성공"),
+            @ApiResponse(code = 408, message = "조회할 수 있는 유저가 1명도 없음"),
+    })
+    @GetMapping("/users")
+    public ListResponseResult<UserInfoListResponseDto> getUserList(){
+        return new ListResponseResult<>(userInfoService.findUserList());
+    }
 
     //관리자 한명 조회
 
@@ -63,5 +72,5 @@ public class UserInfoController {
 
     //관리자 탈퇴 기능?
 
-    //마이페이지 정보 반환환
+    //마이페이지 정보 반환
 }
