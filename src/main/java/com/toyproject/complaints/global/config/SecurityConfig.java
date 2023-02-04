794d59ab79908f480a0682523f700ace899c8059
SSuSG <psg980331@naver.com>
2023-02-04 21:04:07 +0900

    feat : 스웨거 관련 필터 설정

--- src/main/java/com/toyproject/complaints/global/config/SecurityConfig.java
+++ src/main/java/com/toyproject/complaints/global/config/SecurityConfig.java
@@ -10,12 +10,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
@@ -26,6 +27,11 @@ public class SecurityConfig {
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.
                 httpBasic().disable() //rest api이므로 basic auth 및 csrf 보안을 사용하지 않는다는 설정
@@ -33,28 +39,31 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //JWT를 사용하기 때문에 세션을 사용하지 않는다는 설정
                 .and()
                 .authorizeRequests()
-//                .antMatchers("/api/admin/**").hasRole("ADMIN")
-//                .antMatchers("/api/user/**").permitAll()
-//                .antMatchers("/swagger-ui/**", "/v2/api-docs", "/swagger-resources/**",
-//                        "/swagger-ui.html", "/webjars/**", "/swagger/**").permitAll()
                 .antMatchers("/api/admins/**").hasRole("ADMIN")
-                .antMatchers("/swagger-ui/**").permitAll() //해당 API에 대해서는 모든 요청을 허가한다는 설정
                 .antMatchers("/api/users/**").permitAll()
+                .antMatchers("/swagger-resources/**",
+                        "/swagger-ui.html",
+                        "/v2/api-docs",
+                        "/webjars/**").permitAll() //해당 API에 대해서는 모든 요청을 허가한다는 설정
+                //.antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 // exception handling 할 때 우리가 만든 클래스를 추가
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint) //인증 실패시
-                .accessDeniedHandler(jwtAccessDeniedHandler) //인가 실패시
+                .accessDeniedHandler(jwtAccessDeniedHandler)//인가 실패시
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class); //JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행하겠다는 설정
 
         return http.build();
     }
-//
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring().antMatchers( "/swagger-ui/**", "/v2/api-docs", "/swagger-resources/**",
-//                "/swagger-ui.html", "/webjars/**", "/swagger/**");
-//    }
+
+    /*
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers( "/swagger-ui/**", "/v2/api-docs", "/swagger-resources/**",
+                "/swagger-ui.html", "/webjars/**", "/swagger/**");
+    }
+
+     */
 }
