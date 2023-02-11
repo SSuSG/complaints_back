2d1dabafd331ecfda8cff0234c6323726c63f9ae
SSuSG <psg980331@naver.com>
2023-02-11 23:36:05 +0900

    feat : Entity에 변화가 일어나면 실행되는 auditaware 구현 클래스

---
+++ src/main/java/com/toyproject/complaints/global/util/AuditAwareImpl.java
@@ -0,0 +1,27 @@
+package com.toyproject.complaints.global.util;
+
+import com.toyproject.complaints.domain.user.entity.User;
+import com.toyproject.complaints.domain.user.entity.UserDetailsImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Slf4j
+@Component
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        log.info("AuditAwareImpl_getCurrentAuditor -> Entity 변화 일어나면 실행");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (null == authentication || !authentication.isAuthenticated())
+            return null;
+
+        return Optional.of((String)authentication.getPrincipal());
+    }
+}
