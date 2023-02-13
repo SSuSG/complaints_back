6f5633bc8a5de5ba5e420c0e6e896751dc918801
SSuSG <psg980331@naver.com>
2023-02-13 21:37:42 +0900

    refactor : depth줄이고 로직 함수화

--- src/main/java/com/toyproject/complaints/domain/user/service/AccountService.java
+++ src/main/java/com/toyproject/complaints/domain/user/service/AccountService.java
@@ -42,33 +42,28 @@ public class AccountService {
     public Long createUserAccount(CreateUserAccountRequestDto createUserAccountRequestDto) throws ExistEmailException , MessagingException , InValidAccessException , UserNotFoundException {
         log.info("AccountService_createUserAccount -> 관리자에 의한 신규계정 생성");
         User createdAccount = createUserAccountRequestDto.toUserEntity();
-        User curLoginUser = getLoginUser();
 
-        if(Role.ADMIN.equals(curLoginUser.getRole())){
-            if(checkEmailDuplicate(createUserAccountRequestDto.getUserEmail())){
-                throw new ExistEmailException();
-            }else {
-                log.info("임시 비밀번호 생성");
-                String tempPw = createKey();
+        if(checkEmailDuplicate(createUserAccountRequestDto.getUserEmail()))
+            throw new ExistEmailException();
 
-                log.info("계정 성공 생성시 이메일로 임시비밀번호 발송");
-                if(!mailService.sendSimpleMessageForTempPw(createdAccount.getUserEmail() , tempPw)){
-                    throw new MessagingException();
-                }
+        log.info("임시 비밀번호 생성");
+        String tempPw = createKey();
 
-                log.info("생성된 계정의 등록자,수정자,등록일,수정일,비밀번호 초기화");
-                createdAccount.initialUserAndTimeAtCreateAccount(curLoginUser , tempPw);
+        log.info("계정 성공 생성시 이메일로 임시비밀번호 발송");
+        if(!mailService.sendSimpleMessageForTempPw(createdAccount.getUserEmail() , tempPw))
+            throw new MessagingException();
 
-                return userRepository.save(createdAccount).getId();
-            }
-        }else{
-            throw new InValidAccessException();
-        }
+        log.info("생성된 계정의 등록자,수정자,등록일,수정일,비밀번호 초기화");
+        createdAccount.initialUserPw(tempPw);
+
+        return userRepository.save(createdAccount).getId();
     }
 
     public User getLoginUser(){
         log.info("AccountService_getLoginUser -> 현재 로그인 계정 찾기");
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info("저장한 정보1 : {}" , authentication.getDetails());
+        log.info("저장한 정보2 : {}" , authentication.getPrincipal());
         String loginId = authentication.getName();
         User curLoginUser = userRepository.findByUserEmail(loginId).orElseThrow(() -> new UserNotFoundException());
         return curLoginUser;
