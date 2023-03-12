04894172d0936a2c1175a11254306ad6c419f154
SSuSG <psg980331@naver.com>
2023-03-12 23:45:05 +0900

    feat : 관리자 휴대폰 수정 , 마이페이지 정보 조회 컨트롤러 작성

--- src/main/java/com/toyproject/complaints/domain/user/controller/UserInfoController.java
+++ src/main/java/com/toyproject/complaints/domain/user/controller/UserInfoController.java
@@ -2,6 +2,8 @@ package com.toyproject.complaints.domain.user.controller;
 
 import com.toyproject.complaints.domain.user.dto.request.ChangeIpRequestDto;
 import com.toyproject.complaints.domain.user.dto.request.UpdateEmailRequestDto;
+import com.toyproject.complaints.domain.user.dto.request.UpdatePhoneNumberRequestDto;
+import com.toyproject.complaints.domain.user.dto.response.MyPageResponseDto;
 import com.toyproject.complaints.domain.user.dto.response.UserInfoListResponseDto;
 import com.toyproject.complaints.domain.user.dto.response.UserInfoResponseDto;
 import com.toyproject.complaints.domain.user.service.UserInfoService;
@@ -36,7 +38,7 @@ public class UserInfoController {
             @ApiResponse(code = 408, message = "현재 로그인한 관리자를 찾을수 없음"),
             @ApiResponse(code = 410, message = "등록할 수 있는 IP의 수를 초과"),
     })
-    @PostMapping("/ip")
+    @PostMapping("admins/ip")
     public ResponseResult addIpAddress(@Valid @RequestBody ChangeIpRequestDto changeIpRequestDto) {
         if(userInfoService.addIpAddress(changeIpRequestDto))
             return ResponseResult.successResponse;
@@ -50,7 +52,7 @@ public class UserInfoController {
             @ApiResponse(code = 408, message = "현재 로그인한 관리자를 찾을수 없음"),
             @ApiResponse(code = 410, message = "삭제할 수 있는 IP가 존재하지 않음"),
     })
-    @DeleteMapping("/ip")
+    @DeleteMapping("admins/ip")
     public ResponseResult deleteIpAddress(@Valid @RequestBody ChangeIpRequestDto changeIpRequestDto){
         if(userInfoService.deleteIpAddress(changeIpRequestDto))
             return ResponseResult.successResponse;
@@ -64,7 +66,7 @@ public class UserInfoController {
             @ApiResponse(code = 408, message = "현재 로그인한 관리자를 찾을수 없음"),
             @ApiResponse(code = 408, message = "조회할 수 있는 유저가 1명도 없음")
     })
-    @GetMapping("/users")
+    @GetMapping("/admins/users")
     public ListResponseResult<UserInfoListResponseDto> getUserList(){
         return new ListResponseResult<>(userInfoService.getUserList());
     }
@@ -76,13 +78,19 @@ public class UserInfoController {
             @ApiResponse(code = 408, message = "현재 로그인한 관리자를 찾을수 없음"),
             @ApiResponse(code = 408, message = "조회할 수 있는 유저가 1명도 없음")
     })
-    @GetMapping("/users/{userId}")
+    @GetMapping("admins/users/{userId}")
     public SingleResponseResult<UserInfoResponseDto> findUserInfo(@PathVariable Long userId){
         return new SingleResponseResult<>(userInfoService.findUserInfo(userId));
     }
 
-    //관리자 이메일 수정
-    @PostMapping("/users/email")
+    @ApiOperation(value = "관리자 이메일 수정" , notes = "관리자가 유저의 이메일 수정")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "관리자가 유저의 이메일 수정 성공"),
+            @ApiResponse(code = 403, message = "관리자권한이 없는 사용자의 접근"),
+            @ApiResponse(code = 406, message = "이메일 형식이 잘못됌"),
+            @ApiResponse(code = 408, message = "현재 로그인한 관리자를 찾을수 없음")
+    })
+    @PostMapping("admins/users/email")
     public ResponseResult updateEmail(@Valid @RequestBody UpdateEmailRequestDto updateEmailRequestDto){
         if(userInfoService.updateEmail(updateEmailRequestDto)){
             return ResponseResult.successResponse;
@@ -91,9 +99,35 @@ public class UserInfoController {
         }
     }
 
-    //관리자 휴대폰 수정
+    @ApiOperation(value = "관리자 관리자 휴대폰 수정" , notes = "관리자가 유저의 관리자 휴대폰 수정")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "관리자가 유저의 관리자 휴대폰 수정"),
+            @ApiResponse(code = 403, message = "관리자권한이 없는 사용자의 접근"),
+            @ApiResponse(code = 406, message = "휴대폰 형식이 잘못됌"),
+            @ApiResponse(code = 408, message = "현재 로그인한 관리자를 찾을수 없음")
+    })
+    @PostMapping("/admin/user/phoneNumber")
+    public ResponseResult updatePhoneAndEmail(@Valid @RequestBody UpdatePhoneNumberRequestDto updatePhoneNumberRequestDto){
+        if(userInfoService.updatePhoneNumber(updatePhoneNumberRequestDto)){
+            return ResponseResult.successResponse;
+        }else{
+            return ResponseResult.failResponse;
+        }
+    }
+
+    @ApiOperation(value = "마이페이지 정보 반환" , notes = "마이페이지 정보 반환")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "마이페이지 정보 반환 성공"),
+            @ApiResponse(code = 403, message = "접근한 유저페이지와 로그인정보가 일치하지 않는 경우"),
+            @ApiResponse(code = 408, message = "현재 로그인한 관리자를 찾을수 없음")
+    })
+    @GetMapping("/users/{userId}")
+    public SingleResponseResult getMyPageInfo(@PathVariable Long userId){
+        return new SingleResponseResult<MyPageResponseDto>(userInfoService.getMyPageInfo(userId));
+    }
+
+
 
     //관리자 탈퇴 기능?
 
-    //마이페이지 정보 반환
 }
